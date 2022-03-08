0d9bf151372fb4bfa4fa930b71b5a488a6424801
Alberto Panu <alberto@panu.it>
2022-03-08 15:05:42 +0100

    Added database in default.js

--- routes/default.js
+++ routes/default.js
@@ -35,7 +35,8 @@ router.get("/personalInfo", (request, response) => {
 		});
 	} else {
 		let tokenInfo = undefined,
-			userInfo = database.users[request.session.username];
+			//userInfo = database.users[request.session.username];
+			userInfo = database.getData("/users/"+ request.session.username);
 		if (userInfo.oneTimeToken) {            
 			if (userInfo.oneTimeToken.expires > new Date().getTime()) {
 				tokenInfo = { 
