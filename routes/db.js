b740c1ee8fbcb200090fd6bd6e44b89e59ac1184
Alberto Panu <alberto@panu.it>
2022-03-08 14:59:16 +0100

    Added db on db.js

--- routes/db.js
+++ routes/db.js
@@ -1,3 +1,18 @@
+const JsonDB = require('node-json-db').JsonDB
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config
+
+const database = new JsonDB(new Config("myDataBase", true, false, '/'));
+
+try {
+    var data = database.getData("/users");
+} catch(error) {
+    // The error will tell you where the DataPath stopped. In this case test1
+    // Since /test1/test does't exist.
+    console.error("Createt a new db");
+    //database.push("/users","{}");
+};
+
+/*
 let database = {
     users: {
         /* username: {
@@ -8,7 +23,7 @@ let database = {
             'oneTimeToken': undefined,
             'recoveryEmail': undefined
         } */
-    }
+/*    }
 };
-
+*/
 module.exports = database;
