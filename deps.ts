d2dfb20268b1a6d9bd2f3dcf6a91b30a0bfd4cea
Hexagon <hexagon@56k.guru>
2022-04-13 16:26:25 +0200

    Bump

--- deps.ts
+++ deps.ts
@@ -5,7 +5,7 @@ export { crypto } from "https://deno.land/std@0.133.0/crypto/mod.ts";
 export { opine, json, serveStatic, Router } from "https://deno.land/x/opine@2.1.5/mod.ts";
 import { FileDB, Document } from "https://deno.land/x/filedb@0.0.6/mod.ts"; export { FileDB }; export type { Document};
 export { OpineSession } from "https://deno.land/x/sessions@v1.5.4/mod.ts";
-export { Webauthn } from "https://deno.land/x/webauthn@0.9.2/src/deno/webauthn.js";
+export { Webauthn } from "https://deno.land/x/webauthn@0.9.3/src/deno/webauthn.js";
 
 // Third party
 export { base64 } from "https://cdn.jsdelivr.net/gh/hexagon/base64@1/src/base64.js";
