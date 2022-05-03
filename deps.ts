2a796e9ab61d4306526de6cb00ea649f63d4508d
Hexagon <hexagon@56k.guru>
2022-05-04 00:36:28 +0200

    Dependency update

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@
 export { crypto } from "https://deno.land/std@0.134.0/crypto/mod.ts";
 
 // Deno third-party
-import { opine, json, serveStatic, Router, HTTPSOptions, HTTPOptions } from "https://deno.land/x/opine@2.1.5/mod.ts"; 
+import { opine, json, serveStatic, Router, HTTPSOptions, HTTPOptions } from "https://deno.land/x/opine@2.2.0/mod.ts"; 
 export {opine, json, serveStatic, Router};
 export type { HTTPSOptions, HTTPOptions };
 
@@ -11,7 +11,7 @@ export { FileDB };
 export type { Document};
 
 export { OpineSession } from "https://deno.land/x/sessions@v1.5.4/mod.ts";
-export { Webauthn } from "https://deno.land/x/webauthn@0.9.9/dist/webauthn.js";
+export { Webauthn } from "https://deno.land/x/webauthn@0.9.12/dist/webauthn.js";
 
 // Third party
 export { base64 } from "https://cdn.jsdelivr.net/gh/hexagon/base64@1/src/base64.js";
